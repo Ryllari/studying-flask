78b1d3c5092243fa27f4d6415e6da05c8e7a8db3
ryllari.marques <ryllari.marques@conceptu.ind.br>
2020-08-22 21:48:46 -0300

    non-string fields validation

--- app/validators.py
+++ app/validators.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from flask import request
 from functools import wraps
 
@@ -23,6 +25,16 @@ def validate_request_data(f):
             missing_fields = [field for field in number_fields if field not in data]
             if missing_fields and request.method != 'PATCH':
                 raise bad_request_error(f'Missing fields: {missing_fields}')
+
+            # Verify data format
+            try:
+                int(data.get("id", 0))
+                monthy = Decimal(data.get('monthyPrice', '0.00'))
+                setup = Decimal(data.get('setupPrice', '0.00'))
+                if any(t < 0 for t in [monthy, setup]):
+                    raise
+            except Exception as e:
+                raise bad_request_error('Invalid data format')
 
         return f(*args, **kwargs)
     return wrapper
